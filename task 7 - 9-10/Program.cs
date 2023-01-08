52df94181d9b5e27e208a62d6dcf453c6f082e6d
Elvinp329 <elvinkj@code.edu.az>
2023-01-08 17:27:41 +0400

    task7-9-10

---
+++ task 7 - 9-10/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace task_7___9_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  #task7
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] mass = new int[n];
+            //int count = 0;
+
+            //for (int i = 0; i<mass.Length; i++)
+            //{
+
+            //    mass[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+
+            //   if (mass[i] % 7 == 1 || mass[i] % 7 == 2 || mass[i] % 7 == 5) ;
+            //    {
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine($"Massivin 7 bolende qaligi 1,2 ve ya 5 olan elementlerin sayi {count}");
+
+            // #endtask7
+
+
+            Console.Write("Massivin uzunlugunu daxil edin");
+            int n = int.Parse(Console.ReadLine());
+            int[] mass = new int[n];
+
+            int max = mass[0];
+            
+
+            for (int i = 1; i<mass.Length; i++)
+            {
+                mass[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine($"mass[{i}]");
+            }
+            for (int i = 0; i<mass.Length; i++)
+            {
+                if (mass[i]> mass[max])
+                {
+                    max = i;
+
+                }
+            }
+
+            Console.WriteLine($"massivin en boyuyu {max}");
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
